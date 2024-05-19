ae4a7528ff1445f81674a66178f3899f08d30041
NawafISAlmousa <nawaf.i.s.m2@gmail.com>
2024-05-19 18:31:38 +0300

    Added new files and features including the connections between the main page and log in and register etc...

--- SAHAM/sahamapp/models.py
+++ SAHAM/sahamapp/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.core.validators import MinLengthValidator # This function makes sure the data field is not less than a number we specify
 
-class Customer(models.Model):
-    userName = models.CharField(max_length=32,primary_key=True, validators=[MinLengthValidator(3)])
-    password = models.CharField(max_length=32, validators=[MinLengthValidator(6)])
+
 
 class Freelancer(models.Model):
-    freelancerId = models.CharField(max_length=32,primary_key=True, validators=[MinLengthValidator(3)])
+    freelancerId = models.CharField(max_length=6,primary_key=True)
+    email = models.EmailField(max_length=256,unique=True)
     password = models.CharField(max_length=32, validators=[MinLengthValidator(6)])
     description = models.CharField(max_length=500)
     name = models.CharField(max_length=32, validators=[MinLengthValidator(3)])
-    phonenumber = models.CharField(max_length=10, validators=[MinLengthValidator()])
+    phonenumber = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
 
+
+class Service(models.Model):
+    serviceId = models.CharField(max_length=6, primary_key=True)
+    name = models.CharField(max_length=32, validators=[MinLengthValidator(3)])
+    description = models.CharField(max_length=500)
+    price = models.FloatField()
+    freelancer = models.ForeignKey(Freelancer, on_delete=models.CASCADE, related_name='services')
+
+
+class Customer(models.Model): # 
+    userName = models.CharField(max_length=32,primary_key=True, validators=[MinLengthValidator(3)])
+    password = models.CharField(max_length=32, validators=[MinLengthValidator(6)])
+    bookedServices = models.ManyToManyField(Service, blank=True, related_name="customers")
--- SAHAM/sahamapp/views.py
+++ SAHAM/sahamapp/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from .models import Service,Freelancer,Customer
+from django import forms
+import random
+import string
 
-# Create your views here.
+
+def generate_unique_id():
+    length = 6
+    characters = string.ascii_letters + string.digits
+    while True:
+        new_id = ''.join(random.choices(characters, k=length))
+        if not Freelancer.objects.filter(freelancerId=new_id).exists():
+            return new_id
+        
+    
+
+def homePage(request):
+    return render(request,"sahamapp/index.html")
+
+
+
+def registerFreelancer(request):
+    if request.method == "POST":
+        form = request.POST
+        freelancer = Freelancer(freelancerId = generate_unique_id(),
+                                email = form["email"],
+                                password = form["password"],
+                                description = form["description"],
+                                name = form["username"],
+                                phonenumber = form["phonenumber"])
+        freelancer.save()
+        return render(request, 'sahamapp/index.html')
+        
+    # freelancerId = models.CharField(max_length=6,primary_key=True)
+    # email = models.EmailField(max_length=256,unique=True)
+    # password = models.CharField(max_length=32, validators=[MinLengthValidator(6)])
+    # description = models.CharField(max_length=500)
+    # name = models.CharField(max_length=32, validators=[MinLengthValidator(3)])
+    # phonenumber = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+    return render(request,'sahamapp/registerFL.html')
+
+
+def loginFreelancer(request):
+    if request.method == "POST":
+        form = request.POST
+        current = Freelancer.objects.filter(email = form["email"])
+        print(current)
+        if current.exists():
+            if current[0].password == form["password"]:
+                # TEESTING
+                return render(request, "sahamapp/index.html")
+            return render(request, "sahamapp/loginFL.html",{
+                   'error':True
+                })
+            
+        
+    return render(request , "sahamapp/loginFL.html")
+
+def editpage(request):
+    return render(request ,'sahamapp/editPage.html')
