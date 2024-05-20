4597c3b2e07f3eeeff45bb6729387b80481748d7
NawafISAlmousa <nawaf.i.s.m2@gmail.com>
2024-05-20 12:13:33 +0300

    Back main menu button added and app url fixed

--- SAHAM/SAHAM/urls.py
+++ SAHAM/SAHAM/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saham/', include("sahamapp.urls"))
+    path('', include("sahamapp.urls"))
     
 ]
